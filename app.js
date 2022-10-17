ad93f747c835316ef7d1ad9f71ff8f84571feb92
NSMNIA <kevin.stoop@revolveddesign.com>
2022-10-17 17:30:29 +0200

    added button with click event to show notification
    Signed-off-by: NSMNIA <kevin.stoop@revolveddesign.com>

---
+++ app.js
@@ -0,0 +1,16 @@
+const button = document.createElement('button');
+button.textContent = 'Greet me!';
+document.body.insertAdjacentElement('afterbegin', button);
+
+button.addEventListener('click', () => {
+    chrome.runtime.sendMessage('',
+        {
+            type: 'notification',
+            options: {
+                title: 'Just wanted to notify you',
+                message: 'How great it is!',
+                iconUrl: './icons/icon.png',
+                type: 'basic'
+            }
+        });
+});
